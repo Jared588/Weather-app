7f82863b6015a08fa471f644e326f9182a44142d
Jared588 <jaredeprice@gmail.com>
2023-11-06 14:24:23 +0200

    Add searchbar and link it with the API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import RequestAPI from './api';
-import './styles.css'
+import './styles.css';
 
 document.addEventListener('DOMContentLoaded', async () => {
+  // Make default API request
   try {
     const data = await RequestAPI('london');
     console.log(data);
   } catch (error) {
     console.error('Error', error);
   }
-});
 
+  // searchbar
+  const searchbar = document.getElementById('searchbar');
+  searchbar.addEventListener('keydown', async (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      const input = event.target.value.toLowerCase();
+      try {
+        const weatherData = await RequestAPI(input);
+        console.log(weatherData);
+      } catch (error) {
+        console.log('Error', error);
+      }
+    }
+  });
+});
